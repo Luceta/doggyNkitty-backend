af637a2c466f911afc2ef19ab88fb6d258304868
Luceta <57941164+Luceta@users.noreply.github.com>
2022-01-28 16:21:30 +0900

    🎉intialize project settings

---
+++ app.js
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import logger from "morgan";
+import createError from "http-errors";
+import cookieParser from "cookie-parser";
+import routers from "./routes";
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(routers);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/login", function (req, res, next) {
+  res.send("make login router");
+});
+
+export default router;
